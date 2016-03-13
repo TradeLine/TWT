f73d32e1e5f0630ad37ea8e6320602c39a4f0f9a
Subochev <caffeine.mgn@gmail.com>
2016-03-14 01:59:41 +0300

    Убрал логирование

--- libs/json/src/main/java/org/tlsys/twt/json/Json.java
+++ libs/json/src/main/java/org/tlsys/twt/json/Json.java
@@ -1,7 +1,6 @@
 package org.tlsys.twt.json;
 
 import org.tlsys.twt.CastUtil;
-import org.tlsys.twt.Console;
 import org.tlsys.twt.JDictionary;
 import org.tlsys.twt.Script;
 import org.tlsys.twt.annotations.JSClass;
@@ -15,8 +14,6 @@ import java.util.Date;
 @JSClass
 public class Json {
     public static String toJSON(Object obj) {
-        Console.info("try convert object to JSON");
-        Console.dir(obj);
         if (Script.isUndefined(obj))
             return null;
         try {
@@ -32,7 +29,6 @@ public class Json {
             if (cl.isPrimitive()) {
                 return obj.toString();
             }
-            Console.info("-1");
 
             if (cl.isArray()) {
                 String out = "[";
@@ -47,7 +43,6 @@ public class Json {
                 out += "]";
                 return out;
             }
-            Console.info("-2");
 
             if (Iterable.class.isAssignableFrom(cl)) {
                 Iterable im = (Iterable) obj;
@@ -68,30 +63,21 @@ public class Json {
                 return data.getTime()+"";
             }
 
-            Console.info("-3");
 
             String out = "{\"@type\":\"" + cl.getName() + "\"";
             Class clazz = cl;
-            Console.info("-4");
             while (clazz != null) {
-                Console.info("-4.1 class=" + clazz.getName() + " field size=" + clazz.getFields().length);
                 for (Field f : clazz.getFields()) {
                     if (!Modifier.isStatic(f.getModifiers())) {
-                        Console.info("-4.2 convert " + clazz.getName() + "::" + f.getName());
                         out += ",\"" + f.getName() + "\":" + toJSON(f.get(obj));
                     }
                 }
-                Console.info("-4.3");
                 clazz = clazz.getSuperclass();
-                Console.info("-4.4");
                 if (clazz == Object.class)
                     break;
-                Console.info("-4.5");
             }
-            Console.info("-5");
             return out + "}";
         } catch (IllegalAccessException e) {
-            Console.info("ERROR");
             throw new RuntimeException(e);
         }
     }
@@ -102,45 +88,27 @@ public class Json {
     }
 
     public static Object readObject(Object ina, Class needClass, JDictionary<Object> ids) {
-        Console.info("READ=");
-        Console.dir(ina);
-        Console.info("LINE-1 start read...");
         try {
-            Console.info("LINE-1-1 is null?");
             if (ina == null)
                 return null;
-            Console.info("LINE-1-2 is simple type?");
             String type = Script.typeOf(ina);
             if (type.equals("boolean") || type.equals("number") || type.equals("string"))
                 return ina;
-            Console.info("LINE-2 is array?");
             if (Script.code("Array.isArray(", ina, ")")) {
-                Console.info("LINE-2-1");
-                Console.info("Need class=");
-                Console.dir(needClass);
                 if (needClass.isArray()) {
-                    Console.info("LINE-2-2");
                     int arLen = Script.code(ina, ".length");
                     Object ar = Array.newInstance(needClass.getComponentType(), arLen);
-                    Script.code("console.info('2.3')");
                     for (int i = 0; i < arLen; i++) {
-                        Script.code("console.info('2.4')");
                         Array.set(ar, i, readObject(Script.code(ina, "[", i, "]"), needClass.getComponentType(), ids));
-                        Script.code("console.info('2.5')");
                     }
-                    Script.code("console.info('2.6')");
                     return ar;
                 }
-                Script.code("console.info('2.7')");
                 throw new RuntimeException("Not supported array type");
             }
-            Console.info("LINE-3");
             if (Script.code(ina, ".hasOwnProperty('@ref')")) {
                 return ids.get(Script.code(ina, "['@ref']"));
             }
-            Console.info("LINE-4");
             String className = Script.code(ina, "['@type']");
-            Console.info("LINE-5");
             if (needClass == null && (className == null || Script.isUndefined(className))) {
                 throw new RuntimeException("Can't get class name...");
             }
@@ -148,34 +116,28 @@ public class Json {
                 long time = CastUtil.cast(ina);
                 return new Date(time);
             }
-            Console.info("LINE-6");
             Class objClass;
             if (className == null || Script.isUndefined(className)) {
                 objClass = needClass;
             }else {
                 objClass = ClassStorage.get().getByName(className);
             }
-            Console.info("LINE-7");
 
             if (objClass == boolean.class)
                 return Script.code(ina, ".value");
             if (objClass.isArray()) {
                 return readObject(Script.code(ina, "['@items']"), objClass, ids);
             }
-            Console.info("LINE-8=>" + objClass.getName());
             Object o = objClass.newInstance();
-            Console.info("LINE-9");
             if (Script.code(ina, ".hasOwnProperty('@id')")) {
                 int id = Script.code(ina, "['@id']");
                 ids.set(id, o);
             }
 
             Class oo = objClass;
-            Console.info("LINE-10");
             while (oo != null) {
                 Field[] fields = oo.getFields();
                 for (Field f : fields) {
-                    Console.info("Read field " + f.getName() + " with type " + f.getType());
                     if (Script.code(ina, ".hasOwnProperty(", f.getName(), ")")) {
                         Object t = Script.code(ina, "[", f.getName(), "]");
                         f.set(o, readObject(t, f.getType(), ids));
@@ -185,10 +147,8 @@ public class Json {
                 if (oo == Object.class)
                     break;
             }
-            Console.info("LINE-11");
             return o;
         } catch (Throwable e) {
-            Console.info("ERROR2");
             throw new RuntimeException(e);
         }
     }
