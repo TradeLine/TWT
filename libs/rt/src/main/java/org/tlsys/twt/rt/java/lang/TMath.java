5c6e4f24ee2bb1e187e3225ee09993b891738693
Subochev <caffeine.mgn@gmail.com>
2016-03-17 23:31:54 +0300

    Мелкиеи исправления

--- libs/rt/src/main/java/org/tlsys/twt/rt/java/lang/TMath.java
+++ libs/rt/src/main/java/org/tlsys/twt/rt/java/lang/TMath.java
@@ -143,9 +143,11 @@ public final class TMath {
 
     public static long multiplyExact(long x, long y) {
         long r = x * y;
+
         long ax = Math.abs(x);
         long ay = Math.abs(y);
         if (((ax | ay) >>> 31 != 0)) {
+
             // Some bits greater than 2^31 that might cause overflow
             // Check the result using the divide operator
             // and check for the special case of Long.MIN_VALUE * -1
@@ -153,7 +155,9 @@ public final class TMath {
                     (x == Long.MIN_VALUE && y == -1)) {
                 throw new ArithmeticException("long overflow");
             }
+
         }
+
         return r;
     }
 
