b4ebf369c775663079a18b957b27edca2d458f9c
Subochev Anton <caffeine.mgn@gmail.com>
2016-03-24 11:24:09 +0300

    123

--- wf/wildfly-deploy/src/main/java/org/tlsys/wildfly/ConnectSettings.java
+++
@@ -1,8 +0,0 @@
-package org.tlsys.wildfly;
-
-import java.io.IOException;
-import java.net.UnknownHostException;
-
-public interface ConnectSettings {
-    public Connector createConnector() throws IOException;
-}
