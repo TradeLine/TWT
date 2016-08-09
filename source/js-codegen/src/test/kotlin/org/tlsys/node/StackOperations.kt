1cd6c2e07df5c11f9b9f3735955a00d16155339d
Bird <mail@birdlab.ru>
2016-08-09 23:11:37 +0300

    Закоментировал не используемый метод

--- source/js-codegen/src/test/kotlin/org/tlsys/node/StackOperations.kt
+++ source/js-codegen/src/test/kotlin/org/tlsys/node/StackOperations.kt
@@ -1,17 +1,15 @@
 package org.tlsys.node
 
-import org.tlsys.BaseBlock
+open class Push() : Node()
 
-open class Push():Node()
-
-open class PushLdc(val value:Any):Push() {
-    override fun toString(): String{
+open class PushLdc(val value: Any) : Push() {
+    override fun toString(): String {
         return "PushLdc(value=$value)"
     }
 }
-
+/*
 open class PushInt(val value:Int):Push() {
     override fun toString(): String{
         return "PushInt(value=$value)"
     }
-}
+}*/
